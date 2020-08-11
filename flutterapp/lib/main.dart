19e90291f9d8a5b1ec818106dd315ee0831f2f71
Theunis Botha <tbotha95@gmail.com>
2020-08-12 01:24:32 +0200

    Created a stateful widget for random words

--- flutterapp/lib/main.dart
+++ flutterapp/lib/main.dart
@@ -18,10 +18,28 @@ class MyApp extends StatelessWidget {
           title: Text('Flutter Demo Home Page'),
         ),
         body: Center(
-          child: Text(wordPair.asPascalCase),
+          child: RandomWords(),
         ),
       ),
     );
+  }
+}
+
+// StatefulWidget maintains state during lifetime of widget. It also creates a state
+// class. Using `stful` creates boilerplate code for a StatefulWidget.
+// RandomWords stateful widget does little else except create the State class.
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+// Prefix ("_") makes it private and is best practice for State objects.
+// Most of the app's logic resides here.
+class _RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
   }
 }
 
